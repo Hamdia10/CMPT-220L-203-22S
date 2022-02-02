509fe198c5f01248c4680844bf5b6babc10532a5
Hamdia10 <89795252+Hamdia10@users.noreply.github.com>
2022-02-02 15:58:56 -0500

    Finished Lab 2

--- JavaLabs/Lab 3/src/base/Main.java
+++ JavaLabs/Lab 3/src/base/Main.java
@@ -14,10 +14,61 @@ public class Main {
      */
     public static void main(String[] args) {
         // Make an if statement that triggers a print or println statement
+        int x = 47;
+        int y = 78;
+        if (y > x){
+            System.out.println("y is greater than x ");
+        }
         // Make an if, else statement where the else statement triggers a print or println statement
+        int social_hours = 5;
+        if (social_hours > 8){
+            System.out.println("Time to have personal space.");
+        } else {
+            System.out.println("Alright let's talk then.");
+        }
+
         // Make an if, else if, else statement where the else if statement triggers a print or println statement
+        int speed = 67;
+        if (speed > 80){
+            System.out.println("You've exceeded the speed limit!");
+        } 
+        else if (speed < 80) {
+            System.out.println("Your speed is looking good.");
+        } 
+        else {
+            System.out.println("Do you want to turn up the radio?");
+        }
         // Make 2 variables and use them in an if else conditional print from one of the sections
+        String weather = "Sunny" ;
+        String forecast = "Cloudy" ;
+        if ( weather == "Sunny" ){
+            System.out.println("It's a good day to be outside.");
+        } 
+        if ( forecast == "Cloudy" ){
+            System.out.println("It may get a bit cold outside so bring  your coat.");
+        } 
+
+        else {
+            System.out.println("The weather station got it wrong again!");
+        }
         // Make an if statement using 2 variables and an AND(&&) statement that triggers a print or println statement
+        int temp = 88;
+        boolean Sunny = true; 
+        
+        if (temp > 70 && Sunny){
+            System.out.println("The weather is great today lets do something fun !");
+        
+
+        }
+
         // Make an if statement using 2 variables and an OR(||) statement that triggers a print or println statement
+        int work_hours = 3;
+        boolean break_time = true; 
+        
+        if (work_hours > 4 || break_time){
+            System.out.println("It's time to take a 30 minute break from work.");
+        
+
+        }
     }
 }
